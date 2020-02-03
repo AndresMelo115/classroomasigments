a0d8bd9740633461624b76120196f862926c289b
Andres Melo <andresm121@hotmail.com>
2020-02-02 23:03:12 -0300

    This time for sure

---
+++ Ejercicio1/Ejercicio1/Controllers/HomeController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ejercicio1.Models;
+
+namespace Ejercicio_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Captura");
+        }
+
+        [HttpGet]
+        public IActionResult Captura()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Signozod(string nom, int nume1, int nume2)
+        {
+            switch (nume2)
+            {
+                case 1:
+                    if (nume1 <= 20)
+                    {
+                        ViewBag.Resultado = " Capricornio ";
+                    }
+                    else
+                    {
+                        ViewBag.Resultado = " Acuario ";
+                    }
+                    break;
+                case 2:
+                    if (nume1 <= 19)
+                    {
+                        ViewBag.Resultado = " Acuario ";
+                    }
+                    else
+                    {
+                        ViewBag.Resultado = " Piscis ";
+                    }
+                    break;
+                case 3:
+                    if (nume1 <= 20)
+                    {
+                        ViewBag.Resultado = " Piscis ";
+                    }
+                    else
+                    {
+                        ViewBag.Resultado = " Aries ";
+                    }
+                    break;
+                case 4:
+                    if (nume1 <= 20)
+                    {
+                        ViewBag.Resultado = " Aries ";
+                    }
+                    else
+                    {
+                        ViewBag.Resultado = " Tauro ";
+                    }
+                    break;
+                case 5:
+                    if (nume1 <= 21)
+                    {
+                        ViewBag.Resultado = " Tauro ";
+                    }
+                    else
+                    {
+                        ViewBag.Resultado = " Geminis ";
+                    }
+                    break;
+                case 6:
+                    if (nume1 <= 21)
+                    {
+                        ViewBag.Resultado = " Geminis ";
+                    }
+                    else
+                    {
+                        ViewBag.Resultado = " Cancer ";
+                    }
+                    break;
+                case 7:
+                    if (nume1 <= 23)
+                    {
+                        ViewBag.Resultado = " Cancer ";
+                    }
+                    else
+                    {
+                        ViewBag.Resultado = " Leo ";
+                    }
+                    break;
+                case 8:
+                    if (nume1 <= 23)
+                    {
+                        ViewBag.Resultado = " Leo ";
+                    }
+                    else
+                    {
+                        ViewBag.Resultado = " Virgo ";
+                    }
+                    break;
+                case 9:
+                    if (nume1 <= 23)
+                    {
+                        ViewBag.Resultado = " Virgo ";
+                    }
+                    else
+                    {
+                        ViewBag.Resultado = " Libra ";
+                    }
+                    break;
+                case 10:
+                    if (nume1 <= 23)
+                    {
+                        ViewBag.Resultado = " Libra ";
+                    }
+                    else
+                    {
+                        ViewBag.Resultado = " Escorpio ";
+                    }
+                    break;
+                case 11:
+                    if (nume1 <= 22)
+                    {
+                        ViewBag.Resultado = " Escorpio ";
+                    }
+                    else
+                    {
+                        ViewBag.Resultado = " Sagitario ";
+                    }
+                    break;
+                case 12:
+                    if (nume1 <= 21)
+                    {
+                        ViewBag.Resultado = " Sagitario ";
+                    }
+                    else
+                    {
+                        ViewBag.Resultado = " Capricornio ";
+                    }
+                    break;
+
+                default:
+                    ViewBag.Resultado = " Ha colocado una numeracion que no coincide con dias o meses ";
+                    break;
+
+            }
+
+            ViewBag.Nombre = nom;
+
+            return View("Horoscopo");
+        }
+    }
+}
---
+++ Ejercicio2/Ejercicio2/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ejercicio2.Models;
+
+namespace Ejercicio2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Operacion");
+        }
+        [HttpGet]
+        public IActionResult Operacion()
+        {          
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Ecuacion(int nume1, int nume2, int nume3)
+        {
+            
+            var resultadoA = -nume2 + ((Math.Sqrt(((nume2 * nume2) - (4 * nume1 * nume3)))));
+            var resultadoB = -nume2 - ((Math.Sqrt(((nume2 * nume2) - (4 * nume1 * nume3)))));
+            
+            ViewBag.ResultadoA = resultadoA;
+            ViewBag.ResultadoB = resultadoB;
+            ViewBag.divisor = nume1*2;
+
+            return View("Total");
+        }
+
+    }
+}
